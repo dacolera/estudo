7c0ab74c8a7c4c160001e84d7e7a9e6b0944c5dc
dacolera <dacolera360@gmail.com>
2015-07-12 12:17:38 -0300

    adicionando complexidade as classes foo e bar para testar desempenho do design prototype

--- DesignPatterns/Creational/Prototype/BarBookPrototype.php
+++ DesignPatterns/Creational/Prototype/BarBookPrototype.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace DesignPatterns\Creational\Prototype;
+use phpDocumentor\Reflection\DocBlock\Description;
+use phpDocumentor\Reflection\DocBlock\Location;
+use phpDocumentor\Reflection\DocBlock\Serializer;
+use phpDocumentor\Reflection\DocBlock\Context;
 
 /**
  * Class BarBookPrototype
@@ -11,11 +15,22 @@ class BarBookPrototype extends BookPrototype
      * @var string
      */
     protected $category = 'Bar';
+    protected $i1;
+    protected $i2;
+    protected $i3;
+    protected $i4;
 
     /**
      * empty clone
      */
     public function __clone()
     {
+    }
+
+    public function __construct() {
+        $this->i1 = new Context('wfqfwqf');
+        $this->i2 = new Serializer();
+        $this->i3 = new Location();
+        $this->i4 = new Description('sds');
     }
 }
--- DesignPatterns/Creational/Prototype/FooBookPrototype.php
+++ DesignPatterns/Creational/Prototype/FooBookPrototype.php
@@ -1,6 +1,11 @@
 <?php
 
 namespace DesignPatterns\Creational\Prototype;
+use phpDocumentor\Reflection\DocBlock\Context;
+use phpDocumentor\Reflection\DocBlock\Description;
+use phpDocumentor\Reflection\DocBlock\Location;
+use phpDocumentor\Reflection\DocBlock\Serializer;
+
 
 /**
  * Class FooBookPrototype
@@ -8,11 +13,21 @@ namespace DesignPatterns\Creational\Prototype;
 class FooBookPrototype extends BookPrototype
 {
     protected $category = 'Foo';
-
+    protected $i1;
+    protected $i2;
+    protected $i3;
+    protected $i4;
     /**
      * empty clone
      */
     public function __clone()
     {
+    }
+
+    public function __construct() {
+        $this->i1 = new Context('cevbwe');
+        $this->i2 = new Serializer();
+        $this->i3 = new Location();
+        $this->i4 = new Description('sds');
     }
 }
